407808625632bc69882edf6be7eb37bea9c5d477
sir <sir@DESKTOP-KSQ6RIJ>
2022-05-26 19:01:34 +0530

    My Fisrt Git Practice

---
+++ src/Task01/Git01.java
@@ -0,0 +1,31 @@
+package Task01;
+
+public class Git01
+{
+
+	public void add()
+	{
+		int x=10;
+		int y=20;
+		int z=x+y ;
+		
+		System.out.println("Addition "+z );
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) 
+	{
+	Git01 g=new Git01();
+	g.add();
+		
+		
+		
+	}
+	
+	
+	
+}
